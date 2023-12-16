3aa3320af68df3f081e27ccfd455c2b4323c0710
yashsaini7060 <yk18052000@gmail.com>
2023-12-17 01:35:22 +0530

    starting 3 edit and delete are working

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let todoList = [];
 
-function data() {
+function getData() {
   // Get the values from the input fields
-  var todoValue = document.getElementById("todo").value;
-  var dateValue = document.getElementById("date").value;
+  let todoValue = document.getElementById("todo").value;
+  let dateValue = document.getElementById("date").value;
 
   // Validate the input
   if (todoValue.trim() === "") {
@@ -17,17 +17,17 @@ function data() {
   }
 
   // Create an object to represent the to-do item
-  var todoItem = {
+  let todoItem = {
     task: todoValue,
     date: dateValue,
-    status: "none"
+    status: "incomplete"
   };
 
   // Add the to-do item to the array
   todoList.push(todoItem);
 
   // Optionally, you can display or log the array
-  console.log(todoItem);
+  // console.log(todoItem);
 
   // You can perform additional actions here, such as displaying the to-do list on the page.
   // For example, you can create a list element and append it to a container.
@@ -36,52 +36,112 @@ function data() {
 }
 
 function addItem(todoItem) {
+  let parentContainer = document.getElementById("todos-container");
 
-  var todoContainer = document.createElement("div");
+  let todoContainer = document.createElement("div");
   todoContainer.classList.add("todo-item");
 
-  var todoDataContainer = document.createElement("div");
+  let todoDataContainer = document.createElement("div");
   todoDataContainer.classList.add("todo-data");
 
-  var checkbox = document.createElement("input");
+  let checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   checkbox.classList.add("checkbox");
 
-  var taskDescription = document.createElement("p");
-  taskDescription.textContent = todoItem.task;
+  checkbox.addEventListener("change", function () {
+    // Update the status to "completed" when the checkbox is checked
+    if (checkbox.checked) {
+      todoItem.status = "completed";
+      taskDescription.style.textDecoration = "line-through";
+    } else {
+      todoItem.status = "incomplete";
+      taskDescription.style.textDecoration = "none";
+    }
 
-  var editButton = document.createElement("button");
-  editButton.classList.add("edit-btn");
-  editButton.innerHTML = '<img src="./assets/edit.svg" alt="">';
-  editButton.addEventListener("click", function () {
-    // Add your edit logic here
-    alert("Edit button clicked for: " + todoItem.task);
+    // Optionally, you can display or log the updated array
+    console.log(todoList);
   });
 
-  var deleteButton = document.createElement("button");
+  let taskDescription = document.createElement("p");
+  taskDescription.textContent = todoItem.task;
+
+  let deleteButton = document.createElement("button");
   deleteButton.classList.add("delete-btn");
   deleteButton.innerHTML = '<img src="./assets/trash.svg" alt="">';
   deleteButton.addEventListener("click", function () {
-    // Add your delete logic here
-    alert("Delete button clicked for: " + todoItem.task);
+    // Find the index of the todoItem in the todoList
+    let index = todoList.indexOf(todoItem);
+
+    // Remove the todoItem from the todoList
+    if (index !== -1) {
+      todoList.splice(index, 1);
+    }
+
+    // Remove the corresponding HTML element from the DOM
+    parentContainer.removeChild(todoContainer);
+
+    // Optionally, you can display or log the updated array
+    console.log(todoList);
   });
+
+  let saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
+  saveButton.classList.add("save-btn");
+  saveButton.addEventListener("click", function () {
+    // Update todoItem task and due date with the edited content
+    todoItem.task = taskDescription.textContent;
+    todoItem.date = dueDate.textContent;
+
+    // Disable content editing after saving
+    taskDescription.setAttribute("contenteditable", "false");
+    dueDate.setAttribute("contenteditable", "false");
+
+    // Remove the "Save" button and add back the "Edit" button
+    todoDataContainer.appendChild(editButton);
+    todoDataContainer.removeChild(saveButton);
+
+    // Update the todo list
+    console.log(todoList);
+  });
+
+  let editButton = document.createElement("button");
+  editButton.classList.add("edit-btn");
+  editButton.innerHTML = '<img src="./assets/edit.svg" alt="">';
+  editButton.addEventListener("click", function () {
+    // Enable content editing when the "Edit" button is clicked
+    taskDescription.setAttribute("contenteditable", "true");
+    dueDate.setAttribute("contenteditable", "true");
+
+    // Add a "Save" button to save changes
+    todoDataContainer.appendChild(saveButton);
+    // Remove the "Edit" button
+    todoDataContainer.removeChild(editButton);
+  });
+
+  let dueDateText = document.createElement("span");
+  dueDateText.textContent = "Due Date: ";
+
+  let dueDate = document.createElement("span");
+  dueDate.textContent = todoItem.date;
 
   todoDataContainer.appendChild(checkbox);
   todoDataContainer.appendChild(taskDescription);
+  todoDataContainer.appendChild(dueDate);
   todoDataContainer.appendChild(editButton);
   todoDataContainer.appendChild(deleteButton);
 
-  var todoDateContainer = document.createElement("div");
+  let todoDateContainer = document.createElement("div");
   todoDateContainer.classList.add("todo-date");
-  var dueDate = document.createElement("p");
-  dueDate.textContent = "Due Date: " + todoItem.date;
+
+  todoDateContainer.appendChild(dueDateText.cloneNode(true)); // Clone for consistency
+
   todoDateContainer.appendChild(dueDate);
 
   todoContainer.appendChild(todoDataContainer);
   todoContainer.appendChild(todoDateContainer);
 
   // Append the to-do item to the todos-container
-  document.getElementById("todos-container").appendChild(todoContainer);
+  parentContainer.appendChild(todoContainer);
 
   // Optionally, you can display or log the array
   console.log(todoList);
